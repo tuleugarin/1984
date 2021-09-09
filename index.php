6b1b70e0e6143ac819665afbb8cfd3131302f5d8
tuleugarin <88728140+tuleugarin@users.noreply.github.com>
2021-09-09 15:18:26 +0500

    6-upd table

--- index.php
+++ index.php
@@ -20,34 +20,30 @@
     </div>
   </div>
 </nav>
-<table class="table">
+<div class="container">
+	<div class="row">
+		<div class="col-md-8 offset-md-2">
+			<table class="table">
   <thead>
     <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
+      <th scope="col">Id</th>
+      <th scope="col">Title</th>
+      <th scope="col">Action</th>
     </tr>
   </thead>
   <tbody>
     <tr>
       <th scope="row">1</th>
       <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
+      <td>
+      	<a href="">Edit</a>
+      	<a href="">Delete</a>
+      </td>
     </tr>
   </tbody>
 </table>
+		</div>
+	</div>
+</div>
 </body>
 </html>
