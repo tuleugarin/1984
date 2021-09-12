b22194c1310797f70713e18c73311bcf36675525
tuleugarin <88728140+tuleugarin@users.noreply.github.com>
2021-09-12 10:53:17 +0500

    L-9-2 router

--- index.php
+++ index.php
@@ -1,8 +1,24 @@
 <?php
 include 'functions.php';
+// 1. Настроит сервер, чтобы все запросы шли автоматический на страницу/файл index.php
+$routes = [
+	'/' =>  'public/index.php',
+	'/about' => 'public/about.php'
+];
+$route = $_SERVER['REQUEST_URI'];
+
+if (array_key_exists($route, $routes)){
+	dd($routes[$route]);
+}
+else{
+	dd(404);
+}
+
+
+
+
 $db = include 'database/start.php';
 
-//dd($_SERVER);
 $posts = $db->getAll('posts');
 
 // 4.Вывести через foreach;
